d71641e8ccc4d437cafca84bf962c00fe0fdd0af
Breno Nunes <brenontavares@gmail.com>
2018-10-04 17:40:48 -0300

    Corrigindo erro de menu não sumir

--- menu-mobile/js/menu-mobile.js
+++ menu-mobile/js/menu-mobile.js
@@ -6,19 +6,31 @@ var hideMenu = document.querySelector(".closeMenu");
 
 showMenu.addEventListener("click", function () {
     verificarMenu();
+    showUl()
 });
 
-hideMenu.addEventListener("click", function(){
+hideMenu.addEventListener("click", function () {
     verificarMenu();
+    showUl();
 });
 
-function verificarMenu(){
+function verificarMenu() {
     if (nav.classList.contains("show")) {
         nav.classList.remove("show");
         nav.classList.add("hide");
-    }
-    else {
+    } else {
         nav.classList.remove("hide");
         nav.classList.add("show");
+    }
+};
+
+function showUl() {
+    var menuList = document.querySelector(".menuList");
+
+    if (nav.classList.contains("hide")) {
+        menuList.style.display = "none";
+    }
+    if (nav.classList.contains("show")) {
+        menuList.style.display = "flex";
     }
 }
